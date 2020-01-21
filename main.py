92ee2f9a921971b5104804dcefa1c0c46d22ed6c
Ryan Crowley <ryancrowleycode@gmail.com>
2020-01-21 14:57:45 -0600

    created files and classes

--- main.py
+++ main.py
@@ -2,11 +2,24 @@ from cessna import Cessna
 from motorcycle import Zero
 from ram import Ram
 from tesla import Tesla
+from hundai import Hundai
 
+# Practice: Custom Colors and Sounds
+# Define 5 of your favorite vehicles
+# Move all common properties in your vehicles to a new Vehicle class.
+
+# Create an instance of each vehicle.
 fxs = Zero()
 modelS = Tesla()
 mx410 = Cessna()
+bubba = Ram()
+ryanmobile = Hundai()
 
-fxs.drive()
-modelS.drive()
-mx410.drive()
+
+# Define a different value for each vehicle's properties.
+# Create a drive() method in the Vehicle class.
+# Override the drive() method in all the other vehicle classes. Include the vehicle's color in the message (i.e. "The blue Ram drives past. RRrrrrrummbbble!").
+# Create a turn(self, direction) method, and a stop(self) method on Vehicle. Define a basic implementation of each.
+# Override all three of those methods on some of the vehicles. For example, the stop() method for a plane would be to output the message "The white Cessna rolls to a stop after rolling a mile down the runway."
+# Make your vehicle instances perform all three behaviors.
+
